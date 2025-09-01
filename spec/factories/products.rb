1d7148d127ce0aad702c9a778156214eb4ab3aea
Bittu Choudhary <bittuchoudhary007@gmail.com>
2025-09-01 22:24:00 +0530

    test: Add specs to integrate inventory with product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -4,9 +4,10 @@ FactoryBot.define do
     name { "Green tea" }
     price { 3.11 }
     currency { "GBP" }
+    units { 50 }
 
 
-    initialize_with { new(code: code, name: name, price: price, currency: currency) }
+    initialize_with { new(code: code, name: name, price: price, currency: currency, units: units) }
     to_create { |instance| instance }
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -44,4 +44,32 @@ RSpec.describe Product do
       expect(product1.hash).to eq(product2.hash)
     end
   end
+
+  describe 'inventory support' do
+    it 'accepts optional units parameter for inventory tracking' do
+      product = create(:product)
+
+      expect(product.units).to eq(50)
+    end
+
+    it 'supports units with different currency' do
+      product = create(:product, code: 'SR1', name: 'Strawberries', price: 5.00, currency: 'EUR', units: 25)
+
+      expect(product.units).to eq(25)
+      expect(product.price.currency).to eq('EUR')
+    end
+
+    it 'handles zero units correctly' do
+      product = create(:product, code: 'CF1', name: 'Coffee', price: 11.23, units: 0)
+
+      expect(product.units).to eq(0)
+    end
+
+    it 'supports currency as positional and units as keyword argument' do
+      product = create(:product, code: 'SR1', name: 'Strawberries', price: 5.00, currency: 'USD', units: 30)
+
+      expect(product.price.currency).to eq('USD')
+      expect(product.units).to eq(30)
+    end
+  end
 end
